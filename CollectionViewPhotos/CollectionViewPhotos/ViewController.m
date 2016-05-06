d6ed300060a891dc6ef1ad9e0eadf93032b773d3
陈威 <ccww123@yeah.net>
2016-05-06 15:36:50 +0800

    创建个新工程先提交

---
+++ CollectionViewPhotos/CollectionViewPhotos/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CollectionViewPhotos
+//
+//  Created by 陈威 on 16/5/6.
+//  Copyright © 2016年 ZiGuangMeiShiYun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
